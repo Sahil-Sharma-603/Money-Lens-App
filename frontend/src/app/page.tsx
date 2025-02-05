6f287d9b7c04ed7ebadae73d9c4ad03cd3718373
una <una@mayberryfineart.com>
2025-02-05 12:22:17 -0600

    Frontend Setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React from 'react';
+import Link from 'next/link';
 
 export default function Home() {
   const handleClick = async () => {
@@ -23,11 +24,31 @@ export default function Home() {
           border: 'none',
           borderRadius: '5px',
           cursor: 'pointer',
+          marginBottom: '10px'
         }}
         onClick={handleClick}
       >
         Click Me
       </button>
+
+      <br />
+
+      {/* Eventually this can be replaced with a login button */}
+      <Link href="/dashboard">
+        <button
+          style={{
+            backgroundColor: 'blue',
+            color: 'white',
+            padding: '10px 20px',
+            border: 'none',
+            borderRadius: '5px',
+            cursor: 'pointer',
+            marginBottom: '10px'
+          }}
+        >
+          Go to Dashboard 
+        </button>
+      </Link>
     </div>
   );
 }
