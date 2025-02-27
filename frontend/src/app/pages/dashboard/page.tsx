03bcdaa432759eb990a55cfda512021a7ba144dc
una <una@mayberryfineart.com>
2025-02-26 21:27:49 -0600

    add back plaid button

--- frontend/src/app/pages/dashboard/page.tsx
+++ frontend/src/app/pages/dashboard/page.tsx
@@ -6,6 +6,7 @@ import Balance from './components/Balance';
 import Summary from './components/Summary';
 import Transactions from './components/Transactions';
 import BarChartComponent from './components/BarChartComponent';
+import Link from 'next/link';
 
 export default function Dashboard() {
   return (
@@ -32,6 +33,10 @@ export default function Dashboard() {
 
         <div style={{ flex: '1', display: 'flex', flexDirection: 'column', marginLeft: 10 }}>
           <Transactions />
+          {/* This should be moved elsewhere */}
+          <Link className="btn" href="/pages/plaid-setup">
+            Go to Plaid Setup
+          </Link>
         </div>
       </Card>
     </div>
